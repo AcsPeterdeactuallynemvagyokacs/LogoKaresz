dd5864611e28288da5187f5b6d3c248c29bde111
Péter Levente Ács <acsp.levente@gmail.com>
2022-03-03 14:22:42 +0100

    Kezdő void - Ácsé

--- Ácsé.cs
+++ Ácsé.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace LogoKaresz
 {
     public partial class Form1
     {
+        void kezdés(double méret)
+        {
+
+        }
+
     }
 }
